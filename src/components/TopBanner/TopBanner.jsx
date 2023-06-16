6e4ee6bbce7567e466d0de1ef8dd7d18ef9c9608
wilker22 <wilker.silva@codevasf.gov.br>
2023-06-15 21:32:59 -0300

    topbanner

---
+++ src/components/TopBanner/TopBanner.jsx
@@ -0,0 +1,17 @@
+import React, { Component, Fragment } from 'react';
+import '../../assets/css/custom.css';
+import { Container } from 'react-bootstrap';
+
+class TopBanner extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Container className="topFixedBanner">
+
+        </Container>
+      </Fragment>
+    )
+  }
+}
+
+export default TopBanner
