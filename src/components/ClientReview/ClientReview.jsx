5eafd67ebf7a7a4d845625dc80fe7a2a19d6a789
Wilker <wilker22@outlook.com>
2023-06-20 10:54:06 -0300

    carrousel de clientes

--- src/components/ClientReview/ClientReview.jsx
+++ src/components/ClientReview/ClientReview.jsx
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
-import "slick-carousel/slick/slick.css" 
+import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 import Slider from "react-slick";
 
@@ -57,40 +57,38 @@ class ClientReview extends Component {
                     <Slider {...settings}>
                         <div>
 
-                        <Row className="text-center justify-content-center">
-                            <Col lg={6} md={6} sm={12}>
-                                <img className="circleImg" src="https://image.freepik.com/free-photo/handsome-young-man-with-new-stylish-haircut_176420-19637.jpg" />
-                                <h2 className="reviewName">Kazi Ariyan</h2>
-                                <p className="reviewDescription">Hi! I'm Kazi Ariyan. I'm a web developer with a serious love for teaching I am a founder of eLe easy Learning and a passionate Web Developer, Programmer & Instructor.</p>
-                            </Col>
-                        </Row>
-                    </div>
+                            <Row className="text-center justify-content-center">
+                                <Col lg={6} md={6} sm={12}>
+                                    <img className="circleImg" src="https://image.freepik.com/free-photo/handsome-young-man-with-new-stylish-haircut_176420-19637.jpg" />
+                                    <h2 className="reviewName">Kazi Ariyan</h2>
+                                    <p className="reviewDescription">Hi! I'm Kazi Ariyan. I'm a web developer with a serious love for teaching I am a founder of eLe easy Learning and a passionate Web Developer, Programmer & Instructor.</p>
+                                </Col>
+                            </Row>
+                        </div>
 
-                    <div>
-                        <Row className="text-center justify-content-center">
-                            <Col lg={6} md={6} sm={12}>
-                                <img className="circleImg" src="https://image.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg" />
-                                <h2 className="reviewName">Jack Ma</h2>
-                                <p className="reviewDescription">Hi! I'm Kazi Ariyan. I'm a web developer with a serious love for teaching I am a founder of eLe easy Learning and a passionate Web Developer, Programmer & Instructor.</p>
+                        <div>
+                            <Row className="text-center justify-content-center">
+                                <Col lg={6} md={6} sm={12}>
+                                    <img className="circleImg" src="https://image.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg" />
+                                    <h2 className="reviewName">Jack Ma</h2>
+                                    <p className="reviewDescription">Hi! I'm Kazi Ariyan. I'm a web developer with a serious love for teaching I am a founder of eLe easy Learning and a passionate Web Developer, Programmer & Instructor.</p>
 
-                            </Col>
-                        </Row>
-                    </div>
+                                </Col>
+                            </Row>
+                        </div>
 
+                        <div>
+                            <Row className="text-center justify-content-center">
+                                <Col lg={6} md={6} sm={12}>
+                                    <img className="circleImg" src="https://image.freepik.com/free-photo/curly-man-with-broad-smile-shows-perfect-teeth-being-amused-by-interesting-talk-has-bushy-curly-dark-hair-stands-indoor-against-white-blank-wall_273609-17092.jpg" />
+                                    <h2 className="reviewName">Jhon </h2>
+                                    <p className="reviewDescription">Hi! I'm Kazi Ariyan. I'm a web developer with a serious love for teaching I am a founder of eLe easy Learning and a passionate Web Developer, Programmer & Instructor.</p>
 
-
-                    <div>
-                        <Row className="text-center justify-content-center">
-                            <Col lg={6} md={6} sm={12}>
-                                <img className="circleImg" src="https://image.freepik.com/free-photo/curly-man-with-broad-smile-shows-perfect-teeth-being-amused-by-interesting-talk-has-bushy-curly-dark-hair-stands-indoor-against-white-blank-wall_273609-17092.jpg" />
-                                <h2 className="reviewName">Jhon </h2>
-                                <p className="reviewDescription">Hi! I'm Kazi Ariyan. I'm a web developer with a serious love for teaching I am a founder of eLe easy Learning and a passionate Web Developer, Programmer & Instructor.</p>
-
-                            </Col>
-                        </Row>
-                    </div>
-                </Slider>
-            </Container>
+                                </Col>
+                            </Row>
+                        </div>
+                    </Slider>
+                </Container>
             </Fragment >
         )
     }
