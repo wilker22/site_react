03903521411ab4d12c6c221d2c70c53f119a2659
Wilker <wilker22@outlook.com>
2023-07-04 11:12:24 -0300

    endpoint courses

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Fragment } from 'react'
+
 import { Col, Container, Row } from 'react-bootstrap'
 import { Link } from 'react-router-dom/cjs/react-router-dom.min'
 import RestClient from '../../RestAPI/RestClient';
@@ -26,22 +26,22 @@ class Courses extends Component {
         const MyView = MyList.map(MyList => {
 
             return <Col lg={6} md={12} sm={12}>
-                <Row>
-                    <Col lg={6} md={6} sm={12} className="p-2" >
-                        <img className="courseImg" src={MyList.small_img} />
-                    </Col>
+                        <Row>
+                            <Col lg={6} md={6} sm={12} className="p-2" >
+                                <img className="courseImg" src={MyList.small_img} />
+                            </Col>
 
-                    <Col lg={6} md={6} sm={12}>
-                        <h5 className="text-justify serviceName">{MyList.short_title}  </h5>
-                        <p className="text-justify serviceDescription">{MyList.short_description}</p>
-                        <Link className="courseViewMore float-left" to="/coursedetails" >View Details</Link>
+                            <Col lg={6} md={6} sm={12}>
+                                <h5 className="text-justify serviceName">{MyList.short_title}  </h5>
+                                <p className="text-justify serviceDescription">{MyList.short_description}</p>
+                                <Link className="courseViewMore float-left" to="/coursedetails" >Leia mais...</Link>
+                            </Col>
+                        </Row>
                     </Col>
-                </Row>
-            </Col>
         })
 
         return (
-            <Fragment>
+            
                 <Container className="text-center">
                     <h1 className="serviceMainTitle">CURSOS</h1>
                     <div className="bottom"></div>
@@ -49,7 +49,7 @@ class Courses extends Component {
                         {MyView}
                     </Row>
                 </Container >
-            </Fragment >
+            
         )
     }
 }
