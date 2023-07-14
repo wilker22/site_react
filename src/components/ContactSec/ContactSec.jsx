745112aea97a5f7219a3dafd83313adfdeda62e4
Wilker <wilker22@outlook.com>
2023-07-14 17:19:51 -0300

    fixing

--- src/components/ContactSec/ContactSec.jsx
+++ src/components/ContactSec/ContactSec.jsx
@@ -30,9 +30,22 @@ class ContactSec extends Component {
         })
     }
 
+    sendContact(){
+        let name = document.getElementById("name").valeu;
+        let email = document.getElementById("email").valeu;
+        let message = document.getElementById("message").valeu;
+       // alert(name+"/"+email+"/"+message);
+        let jsonObject = {name:name, email:email, message:message}
+
+        RestClient.PostRequest(AppUrl.ContactSend, JSON.stringify(jsonObject)).then(result => {
+            alert(result);
+        }).catch(error => {
+            alert("Error");
+        })
+    }
+
+
     render() {
-
-
         return (
             <Fragment>
                 <Container className='mt-5'>
@@ -40,40 +53,36 @@ class ContactSec extends Component {
                         <Col lg={6} md={6} sm={12}>
                             <Form>
                                 <Form.Group  >
-                                    <Form.Label>Your Name </Form.Label>
-                                    <Form.Control type="text" placeholder="Enter Your Name" />
+                                    <Form.Label>Nome: </Form.Label>
+                                    <Form.Control id="name" type="text" placeholder="Enter Your Name" />
                                 </Form.Group>
 
                                 <Form.Group  >
-                                    <Form.Label>Your Email </Form.Label>
-                                    <Form.Control type="email" placeholder="Enter Your email" />
+                                    <Form.Label>E-mail: </Form.Label>
+                                    <Form.Control id="email" type="email" placeholder="Enter Your email" />
                                 </Form.Group>
 
                                 <Form.Group  >
-                                    <Form.Label>Message  </Form.Label>
-                                    <Form.Control as="textarea" rows={3} />
+                                    <Form.Label>Menssagem  </Form.Label>
+                                    <Form.Control id="message" as="textarea" rows={3} />
                                 </Form.Group>
 
                                 <br /><br />
 
-                                <Button variant="primary" type="submit">
+                                <Button onClick={this.sendContact} variant="primary">
                                     Enviar
                                 </Button>
                             </Form>
-
                         </Col>
 
                         <Col lg={6} md={6} sm={12}>
-                            <h1 className="serviceName">Discuss Now</h1>
-
+                            <h1 className="serviceName">Relatar Agora</h1>
                             <p className="serviceDescription">
                                 {this.state.address}<br></br>
                                 <FontAwesomeIcon icon={faEnvelope} /> Email : {this.state.email}<br></br>
-                                <FontAwesomeIcon icon={faPhone} /> Phone : {this.state.phone}<br></br>
+                                <FontAwesomeIcon icon={faPhone} /> Telefone : {this.state.phone}<br></br>
                             </p>
-
                         </Col>
-
                     </Row>
                 </Container>
             </Fragment >
